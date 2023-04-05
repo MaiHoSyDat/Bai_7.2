ed365eaf920e3e397cefc1a4ea7db08ef2e92e88
MaiHoSyDat <maihosydat@gmail.com>
2023-04-05 14:33:15 +0700

    Bai_7.2.js

---
+++ Bai_7.2.js
@@ -0,0 +1,13 @@
+
+let inputHeight;
+let inputWidth;
+let area;
+
+ inputHeight = prompt("Nhập giá trị chiều cao");
+ inputWidth = prompt("Nhập giá trị chiều dài");
+area = inputWidth * inputHeight
+document.write("Diện tích hình chữ nhật là : " + area);
+
+
+
+
